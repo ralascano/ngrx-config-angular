8211a02d0896e4ce30a387b98267c01470bbdcdf
ralascanog <ralascanog@gmail.com>
2024-11-12 15:46:45 -0500

    feat: devtools redux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './components/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent],
+  imports: [CounterComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
